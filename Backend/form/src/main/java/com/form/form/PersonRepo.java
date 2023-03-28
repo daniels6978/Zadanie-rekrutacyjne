0cc2724c66635717900e6a5683b74640ba7f1a34
Daniel <daniel213160@gmail.com>
2023-03-28 22:14:54 +0200

    frontend

--- Backend/form/src/main/java/com/form/form/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.form.form;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepo extends CrudRepository<Person, Long> {
-}
